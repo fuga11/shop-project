d985e28246b930e9fc25d1808b9199ee337e4e6b
fuga11 <cikiblak35@gmail.com>
2025-03-20 22:37:22 +0300

    add updateCartQuantity function

--- scripts/chekout.js
+++ scripts/chekout.js
@@ -101,5 +101,16 @@ document.querySelectorAll('.js-delete-link')
         `.js-cart-item-container-${productId}`);
       
       container.remove();
+      updateCartQuantity();
     });
   });
+
+  function updateCartQuantity(){
+    let cartQuantity = 0;
+    cart.forEach((item) => {
+      cartQuantity += item.quantity;
+    });
+  
+    document.querySelector('.js-return-to-home-link').innerHTML = cartQuantity;
+  }
+ updateCartQuantity();
