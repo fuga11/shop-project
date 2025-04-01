33161d42fa0edd6d83060d2705e7c7ce6523fadf
fuga11 <cikiblak35@gmail.com>
2025-04-01 21:16:22 +0300

    create tests for add to cart function

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,9 @@
-export let cart = JSON.parse(localStorage.getItem('cart'));
+export let cart;
+
+loadFromStorage();
+
+export function loadFromStorage(){
+  cart = JSON.parse(localStorage.getItem('cart'));
 
 if (!cart){
   cart = [{
@@ -11,7 +16,7 @@ if (!cart){
     deliveryOptionId: '3'
   }];
 }
-
+}
 
 function safeToStorage() {
   localStorage.setItem('cart', JSON.stringify(cart));
