994086b9d734b6b9637da7f843c88a88a9492ae5
fuga11 <cikiblak35@gmail.com>
2025-04-12 19:58:55 +0300

    add promises

--- scripts/chekout.js
+++ scripts/chekout.js
@@ -2,9 +2,23 @@ import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renrderPaymentSummary } from "./checkout/paymentSummary.js";
 import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
 import { loadProduct } from "../data/products.js";
+import { loadCart } from "../data/cart.js";
 //import "../data/cart-class.js";
-loadProduct(() => {
+
+Promise.all([
+  new Promise((resolve) => {
+    loadProduct(() => {
+    resolve('load product complite');
+    });
+  }),
+  new Promise((resolve) => {
+    loadCart(() => {
+      resolve('load cart complite');
+    });
+  }),
+]).then((values) => {
+  console.log(values);
   renderCheckoutHeader();
-  renrderPaymentSummary();
   renderOrderSummary();
+  renrderPaymentSummary();
 });
