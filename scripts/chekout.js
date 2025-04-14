1afe2a37284d164e7794c6733047a9f2001d8e96
fuga11 <cikiblak35@gmail.com>
2025-04-14 22:35:53 +0300

    convert promises to async await

--- scripts/chekout.js
+++ scripts/chekout.js
@@ -5,16 +5,32 @@ import { loadProduct } from "../data/products.js";
 import { loadCart } from "../data/cart.js";
 //import "../data/cart-class.js";
 
+
+async function loadpage() {
+
+  await loadProduct();
+  const value = await new Promise((resolve) => {
+    loadCart(() => {
+      resolve('value3');
+    });
+  });
+  renderCheckoutHeader();
+  renderOrderSummary();
+  renrderPaymentSummary();
+}
+loadpage()
+
+/*
 Promise.all([
   loadProduct(),
-  new Promise((resolve) => {
-    loadCart(() => {
-      resolve('load cart complite');
-    });
-  }),
+    new Promise((resolve) => {
+      loadCart(() => {
+        resolve('load cart complite');
+      });
+    }),
 ]).then((values) => {
   console.log(values);
   renderCheckoutHeader();
   renderOrderSummary();
   renrderPaymentSummary();
-});
+});*/
